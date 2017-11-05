e9fa86b41164e24a96587a8c28df157be3c5ceef
Augustyn Binkowski <augustyn.mikolaj@gmail.com>
2017-11-05 09:29:03 +0100

    Angular project stetup

--- src/angular/road-story-tracking/src/app/app.module.ts
+++ src/angular/road-story-tracking/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AngularMaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
